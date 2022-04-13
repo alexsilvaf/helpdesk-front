e151626a5fa6c7c942d738f4989732f4f5adc26b
Alex da Silva Ferreira <alex120999@hotmail.com.br>
2022-04-12 22:04:50 -0300

    Rota para componente CHAMADO-READ

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -15,6 +15,7 @@ import {ClienteDeleteComponent} from "./components/cliente/cliente-delete/client
 import {ChamadoListComponent} from "./components/chamado/chamado-list/chamado-list.component";
 import {ChamadoCreateComponent} from "./components/chamado/chamado-create/chamado-create.component";
 import {ChamadoUpdateComponent} from "./components/chamado/chamado-update/chamado-update.component";
+import {ChamadoReadComponent} from "./components/chamado/chamado-read/chamado-read.component";
 
 const routes: Routes = [
     {path: 'login', component: LoginComponent},
@@ -22,19 +23,20 @@ const routes: Routes = [
         path: '', component: NavComponent, canActivate: [AuthGuard], children: [
             {path: 'home',                  component: HomeComponent },
 
-            { path: 'tecnicos',             component: TecnicoListComponent },
+            { path: 'tecnicos',               component: TecnicoListComponent },
             { path: 'tecnicos/create',      component: TecnicoCreateComponent },
             { path: 'tecnicos/update/:id',  component: TecnicoUpdateComponent },
             { path: 'tecnicos/delete/:id',  component: TecnicoDeleteComponent },
 
-            { path: 'clientes',             component: ClienteListComponent },
+            { path: 'clientes',               component: ClienteListComponent },
             { path: 'clientes/create',      component: ClienteCreateComponent },
             { path: 'clientes/update/:id',  component: ClienteUpdateComponent },
             { path: 'clientes/delete/:id',  component: ClienteDeleteComponent },
 
-            { path: 'chamados',             component: ChamadoListComponent },
+            { path: 'chamados',               component: ChamadoListComponent },
             { path: 'chamados/create',      component: ChamadoCreateComponent },
-            { path: 'chamados/update/:id',  component: ChamadoUpdateComponent }
+            { path: 'chamados/update/:id',  component: ChamadoUpdateComponent },
+            { path: 'chamados/read/:id',      component: ChamadoReadComponent }
         ]
     }
 ];
